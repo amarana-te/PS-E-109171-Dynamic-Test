c3b4df9acf2a4fd5c371acfac80fd209d0bfe37c
dmagalla-te <dmagalla@thousandeyes.com>
2024-08-16 18:53:40 -0600

    Ready for demo v2.0

--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
-from Operations import read_files, get_info, turn_off_tests, provision_agents
+from Operations import read_files, get_info, update_tests, disable_tests
+import json
 
 OAUTH = ""
 
@@ -9,16 +10,34 @@ headers = {
 
 def main(directory_path):
 
-    #leer jsons para obtener toda la info guardada
+   
     cvs_agents = read_files(directory_path)
-    
-    #obtenemos info de TE
-    cvs_agents = get_info(headers, data=cvs_agents)
+    print(f'Data gathered from the .json file: {cvs_agents} \n ')
+    print("===========================================================")
 
-    cvs_agents = provision_agents(cvs_agents, headers)
+    if cvs_agents:
+
+        #obtenemos info de TE
+        print(f'The information on the JSON was updated. New config will be pushed... \n')
+
+        cvs_agents = get_info(headers, data=cvs_agents)
+
+        print("===========================================================")
+
+        print(f'This is the assignment information for this agent: \n{json.dumps(cvs_agents,indent=4)} \n')
+
+        print("===========================================================\n")
+
+        cvs_agents = update_tests(cvs_agents, headers)
+
+        print("\n===========================================================")
+        print(f'\n Unassign agents from previous run...\n')
+        disable_tests(cvs_agents, headers)
+
+        print("\n===========================================================")
 
 
-    turn_off_tests(cvs_agents, headers)
+        
 
 
 main(directory_path="cvs_folder/")
--- Operations.py
+++ Operations.py
@@ -1,7 +1,7 @@
 import os
-import json
+import json, time
 from datetime import datetime
-from Connector import get_data, post_data, put_data
+from Connector import get_data, put_data
 
 BASE_URL = "https://api.thousandeyes.com/v7/"
 endp_url1 = f"{BASE_URL}account-groups"
@@ -10,13 +10,15 @@ endp_url3 = f"{BASE_URL}tests/http-server"
 
 def get_info(headers:dict, data:dict):
 
-    print('Get Info function')
+    print('+ get_info function \n')
     
     _, account_groups = get_data(headers, endp_url1, params={})
 
     if "accountGroups" in account_groups:
 
         for aid in account_groups['accountGroups']:
+
+            print(f"    Gathering data for this ag: {aid.get('accountGroupName')}\n")
 
         #print("AID: ", aid.get("aid"))
             status, agents = get_data(headers, endp_url2, params={"aid":aid.get("aid"), "agentTypes":"enterprise,enterprise-cluster"})
@@ -27,23 +29,61 @@ def get_info(headers:dict, data:dict):
 
                     if agent["agentName"] == data.get("name"):
 
+                        print(f"    ----> {data.get('name')} agent was found at {aid.get('accountGroupName')} \n")
+
                         data.update({"agentId":agent.get("agentId"), "aid":aid.get("aid")})
 
-                        _, tests = get_data(headers, endp_url3, params={"aid":aid.get("aid")})
+                        status, tests = get_data(headers, endp_url3, params={"aid":aid.get("aid")})
 
-                        if "tests" in tests:
+                        if status == 200:
 
                             tests_list = []
                             remove_tests = []
+
                             for test in tests["tests"]:
                     
-                                if test.get("url") in data.get("urls") and data.get:
+                                if test.get("url") in data.get("urls") and data:
 
-                                    tests_list.append({"testId":test.get("testId"), "testDescription": test.get("description")})
 
-                                elif test.get("url") not in data.get("urls") and data.get:
+                                    tests_list.append({"testId":test.get("testId"), "testDescription": test.get("description"),"enabled":test.get("enabled")})
 
-                                    remove_tests.append({"testId":test.get("testId"), "testDescription": test.get("description")})
+                                elif test.get("url") not in data.get("urls") and data:
+
+
+                                    #consultar los gentes --- el agente por asignar, vive en este tests?
+                                    details_endp = f'{BASE_URL}tests/http-server/{test.get("testId")}'
+                                    params = {'aid':aid.get("aid"), 'expand':'agent'}
+                                    status, test_details = get_data(headers=headers, endp_url=details_endp, params=params)
+
+
+                                    if status == 200:
+
+                                        platform_agents = []
+
+                                        for platform_agent in test_details['agents']:
+
+                                            platform_agents.append(platform_agent['agentId'])
+
+                                        # si el agente vive en ese test:
+                                        if agent.get("agentId") in platform_agents and len(platform_agents) == 1:
+
+                                            #   Es el unico agente ahi? -- si si, tenemos que dejarlo y deshabilitar ese test
+                                            remove_tests.append({"testId":test.get("testId"), "testDescription": test.get("description"), "agents": []}) ## validacion mas abajo: si no hay agentes, solo apaga el test
+
+                                        if agent.get("agentId") in platform_agents and len(platform_agents) > 1:
+
+                                            platform_agents.remove(agent.get("agentId"))
+                                            
+                                            new_agents = []
+
+                                            for ag in platform_agents:
+                                                
+                                                new_agents.append({"agentId": ag})
+                                            
+                                            #   Si no es el unico agente, tenemos que updatear el test -- agents = agents - current_agent
+                                            remove_tests.append({"testId":test.get("testId"), "testDescription": test.get("description"), "agents": new_agents})
+
+                                            
 
                             data.update({"tests": tests_list, "toRemove": remove_tests})
 
@@ -53,29 +93,147 @@ def get_info(headers:dict, data:dict):
 
 
 
-
 # Function to read all JSON files in a folder
 def read_files(directory_path: str) -> dict:
+    # Get today's date in YYYY-MM-DD format
+    today = time.strftime('%Y-%m-%d')
 
     for filename in os.listdir(directory_path):
-
         if filename.endswith('.json'):
-
             file_path = os.path.join(directory_path, filename)
 
-            with open(file_path, 'r') as file:
+            # Get the last modification time of the file
+            mod_time = os.path.getmtime(file_path)
+            mod_time_str = time.strftime('%Y-%m-%d', time.localtime(mod_time))
+            
+            # Check if the file was modified today
+            if mod_time_str == today:
+                # Read and return the JSON data
+                with open(file_path, 'r') as file:
+                    data = json.load(file)
+                return data
+            else:
+                # File was modified on a different date
+                return {}
+    
+    # If no JSON files are found or none are modified today, return an empty dictionary
+    print("No JSON files found or none modified today.")
+    return {}
+
+
+
+
+
+
+# NEW function to update the tests configuration
+def update_tests(cvs_agents:dict, headers:dict):
+
+    print('+ update_tests function \n')
+
+    todays_date = datetime.now().strftime("%Y-%m-%d")
+
+    for test in cvs_agents.get("tests"):
+
+        ## Si esta disable el test al que llegamos -- el unico agente que vivira ahi sera el de mi json
+        if not test.get("enabled"):
+
+            url = f"{BASE_URL}tests/http-server/{test.get('testId')}?aid={cvs_agents.get('aid')}"
+            payload = {"agents": [{"agentId": cvs_agents.get("agentId")}], "enabled": True, "description": todays_date}
+
+            status, provision = put_data(headers, url, json.dumps(payload))
+
+            if status == 200 or status == 201:
+
+                print(f"    Test {test['testId']} was enabled successfully and agent {cvs_agents['name']} was assigned.")
+            
+            else:
+
+                print(f"    Test {test['testId']} failed to be enabled and agent {cvs_agents['name']} not assigned. Reason: {provision}")
+
+
+        # si el test esta enabled -- significa que los agentes de ahi son validos entonces me agrego
+        elif test.get("enabled"):
+
+            url = f'{endp_url3}/{test.get("testId")}' 
+            status, get_test_details = get_data(headers, url, params={"aid":cvs_agents.get("aid"), "expand": "agent"})
+
+            if status == 200 and "agents" in get_test_details:
+
+                new_agents = []
+                new_agents.append({"agentId": cvs_agents.get("agentId")})
+
+                for agent in get_test_details.get("agents"):
+
+                    new_agents.append({"agentId": agent.get("agentId")})
+
+                payload = {"agents": new_agents, "enabled": True, "description": todays_date}
+
+                status, provision = put_data(headers, url, json.dumps(payload))
+
+                if status == 200 or status == 201:
+
+                    print(f"    Test {test['testId']} updated successfully, agent {cvs_agents['name']} was added.")
                 
-                data = json.load(file)
+                else:
+
+                    print(f"    Test {test['testId']} couldn't be updated, no agent added to it. Reason: {provision}")
+
+
+    return cvs_agents
+
+
+
+def disable_tests(cvs_agents:dict, headers:dict):
+
+    print('+ disable_tests function \n')
+
+    if "toRemove" in cvs_agents:
+
+        for test in cvs_agents.get("toRemove"):
+
+            if not test.get('agents'):
+
+                url = f"{BASE_URL}tests/http-server/{test.get('testId')}?aid={cvs_agents.get('aid')}"
+                payload = {"enabled": False}
+
+                status, provision = put_data(headers, url, json.dumps(payload))
+
+                if status == 200 or status == 201:
+
+                    print(f"    Test {test['testId']} was disabled, agent {cvs_agents['name']} 'removed'.")
                 
-            return data
-        
-        else:
+                else:
 
-            return {}
+                    print(f"    Test {test['testId']} couln'd be disabled. Reason: {provision}")
+            
+            elif test.get('agents'):
+
+                url = f"{BASE_URL}tests/http-server/{test.get('testId')}?aid={cvs_agents.get('aid')}"
+                payload = {"enabled": True, "agents":test.get('agents')}
+
+                print(f'PAYLOAD TO REMOVE SECOND CONDITION')
+
+                status, provision = put_data(headers, url, json.dumps(payload))
+
+                if status == 200 or status == 201:
+
+                    print(f"    Test {test['testId']} was updated, agent {cvs_agents['name']} totally removed from the test.")
+
+                else:
+
+                    print(f"    Test {test['testId']} could't be updated, agent {cvs_agents['name']} still lives there. Reason: {provision}")
 
 
 
 
+
+
+
+################################
+#           OLD FUNCTIONS 
+################################
+
+# OLD function to update the tests config 
 def provision_agents(cvs_agents:dict, headers:dict):
 
     todays_date = datetime.now().strftime("%Y-%m-%d")
